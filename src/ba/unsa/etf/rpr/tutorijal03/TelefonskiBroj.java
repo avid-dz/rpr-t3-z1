6a84052025dc3ca7ca2cb25707e32545d9f20b9d
Dzavid <dbrdar1@etf.unsa.ba>
2018-10-30 20:17:59 +0100

    Dodan komentar za klasu TelefonskiBroj

--- src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
+//ovu klasu ni ne treba implementovati ovdje jer je apstraktna
+//iz ove klase izvedene su jos tri klase
 public abstract class TelefonskiBroj {
 
     public abstract String ispisi();
